e474d86d25cee8f2bf810abd508bccd8f90fb44d
Sergey <Zaikouski.sergey@gmail.com>
2023-05-21 14:26:39 +0300

    file 21.05 with select

--- src/components/AddForm.tsx
+++ src/components/AddForm.tsx
@@ -31,6 +31,11 @@ export const AddForm: React.FC<PropsType> = ({watchListId, addFilm}) => {
     if (newFilm.rating > 100 || newFilm.rating < 0) {
         buttonDisabled = true
     }
+    const onChangeSelectHandler = (e:ChangeEvent<HTMLSelectElement>)=>{
+        setNewFilm({...newFilm, genre: e.currentTarget.value})
+
+
+    }
 
     return (
         <>
@@ -41,8 +46,18 @@ export const AddForm: React.FC<PropsType> = ({watchListId, addFilm}) => {
                             property={'name'}/>
                 <SuperInput value={newFilm.rating} type={'number'} newFilm={newFilm} setNewFilm={setNewFilm}
                             property={'rating'}/>
-                <SuperInput value={newFilm.genre} type={'text'} newFilm={newFilm} setNewFilm={setNewFilm}
-                            property={'genre'}/>
+               {/* <SuperInput value={newFilm.genre} type={'text'} newFilm={newFilm} setNewFilm={setNewFilm}
+                            property={'genre'}/>*/}
+                <select name="movies" onChange={onChangeSelectHandler}>
+                    <option value="All">All Genre</option>
+                    <option value="Drama">Drama</option>
+                    <option value="Crime">Crime</option>
+                    <option value="Action">Action</option>
+                    <option value="Military">Military</option>
+                    <option value="Fantasy">Fantasy</option>
+                </select>
+
+
                 <SuperButton name={'ADD'} onClickCallBack={addFilmHandler} disabled={buttonDisabled}/>
             </div>
             }
